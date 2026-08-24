fabda213c1d3c7163472f17e6b28f12822887e6f
gain-0525 <kimgain0525@sookmyung.ac.kr>
2026-08-24 15:43:41 +0900

    [Feat] #3 ActivitySection 구현 완료

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '../components/home/HeroSection';
 import IntroSection from '../components/home/IntroSection';
+import ActivitySection from '../components/home/ActivitySection';
 
 const HomePage = () => {
     return (
         <>
             <HeroSection />
             <IntroSection />
+            <ActivitySection />
         </>
     )
 }
